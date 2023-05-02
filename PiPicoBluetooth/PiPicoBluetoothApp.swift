8a722f358834eded1e57ecffe406dc321678dc45
Abijah Migisho <Abijah.Migisho@noveogroup.com>
2023-05-02 13:49:00 +0300

    Initial Commit

---
+++ PiPicoBluetooth/PiPicoBluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiPicoBluetoothApp.swift
+//  PiPicoBluetooth
+//
+//  Created by Abijah Kajabika on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PiPicoBluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
